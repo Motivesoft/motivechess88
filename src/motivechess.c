06adbf4c67bc5db91948bb3a91a10fbbcea1a2c5
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2023-11-07 23:56:46 +0100

    Initial project

---
+++ src/motivechess.c
@@ -0,0 +1,10 @@
+// motivechess.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+// This project will be C, rather than C++, to minimize bloat and encourage inventiveness
+
+#include <stdio.h>
+
+int main()
+{
+    printf( "Hello World!\n" );
+}
